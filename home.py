21244b1ebf54a4b58b1b8bd855aef4ff086aef54
DanieleMar <daniele.mariotto@gmail.com>
2020-07-21 20:57:16 +0200

    bbo with flask. first complete commit

--- home.py
+++ home.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import json
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello world'
+def create_app():
+  app = Flask(__name__)
+  Bootstrap(app)
+
+  return app
+
+
+readjsonfile= open("open.json", "r") #apri file json
+
+listajson=json.load(readjsonfile) #carica in variabile json
+listaChiuseJson=json.load(open("chiuse.json", "r"))
+
+@app.route("/")
+
+def load_page():
+    return render_template('bbo-home.html', listaAperte=listajson["biblio"], listaChiuse=listaChiuseJson["chiuse"]) 
+
+
+# TEST PER IMPARARE I FLASK TEMPLATES #
+lista1=["abaco", "lemure", "ciuberter"]
+@app.route("/test")
+def load_pg():
+    name= "gianni"
+    return render_template("test.html", nome=name, navigation=lista1) 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ test3.py
@@ -0,0 +1,4 @@
+#FILE di test per provare pezzi di codice 
+
+readjsonfile= open("open.json", "r") #apri file json
+print(readjsonfile.readline)
