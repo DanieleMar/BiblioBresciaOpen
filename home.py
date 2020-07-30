665cc5254e13139420903ac3eaddf7dbe6005bfe
DanieleMar <daniele.mariotto@gmail.com>
2020-07-30 20:37:54 +0200

    deploy iniziale

--- home.py
+++ home.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 import json
 from flask_bootstrap import Bootstrap
 
+#import per ricorrente
+""" import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+ """
 app = Flask(__name__)
 
 def create_app():
@@ -16,7 +21,7 @@ readjsonfile= open("open.json", "r") #apri file json
 listajson=json.load(readjsonfile) #carica in variabile json
 listaChiuseJson=json.load(open("chiuse.json", "r"))
 
-@app.route("/")
+@app.route("/bbo")
 
 def load_page():
     return render_template('bbo-home.html', listaAperte=listajson["biblio"], listaChiuse=listaChiuseJson["chiuse"]) 
@@ -32,3 +37,16 @@ def load_pg():
 if __name__ == '__main__':
     app.run(debug=True)
 
+
+#funzione per ricorrenza
+""" 
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=3)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown()) """
