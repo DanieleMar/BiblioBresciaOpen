890138a7ab3c4bc3e523e313babd2f1d7e915ed4
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-08-14 16:23:43 +0200

    ampliato tempo cronjob a ogni 20 ore

--- cronjob.py
+++ cronjob.py
@@ -6,6 +6,6 @@ import scrapercitta
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(scrapercitta, "interval", seconds=60)
+scheduler.add_job(scrapercitta, "interval", hours=20)
 
 scheduler.start()
