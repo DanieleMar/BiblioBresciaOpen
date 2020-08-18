5866c334ca6beedf5b69ed216260f46aeef1564b
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-08-18 21:27:50 +0200

    modifiche per AWS S3

--- home.py
+++ home.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
 import json
 from flask_bootstrap import Bootstrap
+import boto3
+from config import S3_BUCKET, S3_KEY, S3_SECRET
+
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET
+)
+
+
 
 #import per ricorrente
 """ import time
@@ -9,8 +20,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
  """
 app = Flask(__name__)
 
+
 def create_app():
   app = Flask(__name__)
+
   Bootstrap(app)
 
   return app
@@ -33,6 +46,16 @@ lista1=["abaco", "lemure", "ciuberter"]
 def load_pg():
     name= "gianni"
     return render_template("test.html", nome=name, navigation=lista1) 
+
+
+@app.route('/files')
+def files():
+    s3_resource = boto3.resource('s3')
+    my_bucket = s3_resource.Bucket(S3_BUCKET)
+    summaries = my_bucket.objects.all()
+
+    return render_template('files.html', my_bucket=my_bucket, files=summaries)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
