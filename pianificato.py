6e9a01d7119494463bb0f56c06c1f6d2d9d6c959
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-09-02 12:05:41 +0200

    test di pianificazione di scrapercitta

---
+++ pianificato.py
@@ -0,0 +1,17 @@
+from datetime import datetime 
+from threading import Timer 
+import scrapercitta 
+x=datetime.today() 
+print(x) 
+#y=x.replace(day=x.day+1, hour=4, minute=30, second=0, microsecond=0) giusto, ogni giorno alle 3 
+y=x.replace(day=x.day, hour=12, minute=6, second=0, microsecond=0) 
+delta_t=y-x 
+print(y) 
+print(delta_t) 
+secs=delta_t.seconds+1 
+print(secs) 
+def scraper(): 
+    print ("hello world") 
+    scrapercitta.run() #esegui file python test.py 
+t = Timer(secs, scraper) 
+t.start()
