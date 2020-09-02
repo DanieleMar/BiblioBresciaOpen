7075f1502e84d58b33b73325745660a680c2433f
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-09-02 10:53:16 +0200

    aggiunto scheduler per scraper

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,30 @@
 import schedule
 import time
+import scrapercitta
+import threading
 
 def job():
     print("I'm working...")
+    scrapercitta.run()
 
-schedule.run_continuously()
 
-schedule.every(2).minutes.do(job)
-schedule.every().hour.do(job)
-schedule.every().day.at("10:30").do(job)
+# while 1:
+#     schedule.run_pending()
+#     time.sleep(1)
 
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+class ScheduleThread(threading.Thread): #avvia thread per scheduler in background. 
+    def __init__(self, *pargs, **kwargs):
+        super().__init__(*pargs, daemon=True, name="scheduler", **kwargs)
+
+    def run(self):
+        
+        schedule.every().seconds.do(job)
+        schedule.every().minutes.do(job)
+        schedule.every().hour.do(job)
+        schedule.every().day.at("10:30").do(job)
+        
+        while True:
+            schedule.run_pending()
+            time.sleep(schedule.idle_seconds())
+
+ScheduleThread().start()
