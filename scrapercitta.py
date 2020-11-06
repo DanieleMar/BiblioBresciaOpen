a446a927d7d9dea2fabeec3dfb9e88766c389b9d
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-11-06 17:49:02 +0100

    migliorati commenti

--- scrapercitta.py
+++ scrapercitta.py
@@ -41,14 +41,15 @@ def run():
             return e  # eccezione per url vuoto
         try:
             bs = BeautifulSoup(html.read(), 'html.parser')
-  
-            elem = bs.title  # inutile @@elimino
+
             
-            nomebiblio= bs.find("h2", property="name").get_text() ##Ottengo dalla pagina il nome della biblio 
-            # prelevo orario di oggi
+            nomebiblio= bs.find("h2", property="name").get_text() ##Ottiene dalla pagina il nome della biblio 
+            
+            
+            # Preleva orario di oggi
             try:
-                for sibling in bs.find('div', id="timetable").td.next_sibling.next_sibling: # prelevo primo orario
-                 
+                for sibling in bs.find('div', id="timetable").td.next_sibling.next_sibling: # preleva il primo orario della giornata
+                
                     if ("Chiusa" in sibling):
                         return "chiusa" # ritorna chiusa se la biblioteca è chiusa
                     else:
@@ -57,43 +58,45 @@ def run():
 
                         return sibling+' \n '+pomeriggio.strip() # restituisco l'orario 
             except AttributeError as e:
-                        ### Elenca biblio senza tabella e aggiungo a file json.
-                        ### Utile per avere sott'occhio subito quali biblioteche hanno cancellato l'orario
+                        # Elenca biblio senza tabella e aggiunge a file json.
+                        # Utile per avere sott'occhio subito quali biblioteche hanno cancellato l'orario
                         if (str(e)== "'NoneType' object has no attribute 'td'"): 
                             
 
-                            biblio_senza_orario.append(nomebiblio) ##per elenco di biblio chiuse
+                            biblio_senza_orario.append(nomebiblio) 
                             return None
+
+                        else:
+                            print (e)
 
 
             
         except AttributeError as e:
             return e 
-        print(elem) 
-        return elem #  capire se posso toglierla 
+
 
 
     biblio_aperte_dict = {}
     biblio_chiuse_dict = {}
     
-    # creo dict con orari
+    # crea dizionario con orari
     for i in Biblioteche_citta:
 
-        orario = ottieni_orario(Biblioteche_citta[i]) 
+        orario = ottieni_orario(Biblioteche_citta[i]) #chiama funzione sopra. Biblioteche_citta[i] è l'url
         try:
             if "chiusa" in orario:
-                biblio_chiuse_dict.setdefault('chiuse', []) # inserisco elemento radice in file json. rende più immediato l'inserimento delle biblioteche chiuse
-         
+                biblio_chiuse_dict.setdefault('chiuse', []) # inserisce l'elemento radice in file json. Rende più immediato l'inserimento delle biblioteche chiuse
+
                 biblio_chiuse_dict['chiuse'].append(
                     {'nome': i, 'url': Biblioteche_citta[i].split("/timetable/").pop(0)})
-                    # ottengo url della biblioteca utilizzato per il link cliccabile dall'utente.
+                    # ottiene l'url della biblioteca utilizzato per il link cliccabile dall'utente.
                     # pop elimina lo spazio bianco dalla lista
         
             
         
             else:
 
-                biblio_aperte_dict.setdefault('biblio', []) # creo il dizionario che salva solo le biblioteche aperte oggi
+                biblio_aperte_dict.setdefault('biblio', []) # crea il dizionario che salva solo le biblioteche aperte oggi
                 
                 biblio_aperte_dict['biblio'].append({'nome': i, 'orario': orario.strip().split(
                     "\n"), "url": Biblioteche_citta[i].split("/timetable/").pop(0)}) # core element: inserisce nel dizionario il nome, l'orario e l'url. strip rimuove gi spazi bianchi. split("\n") separa in due campi separati gli orari doppi. 
@@ -104,8 +107,8 @@ def run():
         except TypeError as e:
             print (e)
 
-    biblio_aperte_json = json.dumps(biblio_aperte_dict)  # trasformo dizionario in json
-    biblio_chiuse_json = json.dumps(biblio_chiuse_dict)  # trasformo dizionario in json
+    biblio_aperte_json = json.dumps(biblio_aperte_dict)  # trasforma dizionario in json
+    biblio_chiuse_json = json.dumps(biblio_chiuse_dict)  # trasforma dizionario in json
     runninglog = "\n" + str(datetime.now())  # log di esecuzione
 
 
@@ -113,18 +116,18 @@ def run():
     f1.write(runninglog)
     f1.close
 
-    # creo json aperte
+    # crea json di biblio aperte
     f2 = open("dati/open.json", "w")
     f2.write(biblio_aperte_json)
     f2.close
 
 
-    # creo json chiuse
+    # crea json chiuse
     f3 = open("dati/chiuse.json", "w")
     f3.write(str(biblio_chiuse_json))
     f3.close
 
-    # txt con tutte le biblioteche senza orario sul sito
+    # txt con tutte le biblioteche senza orario sul sito Opac
     f4 = open("dati/biblio_orari_assenti.txt", "w")
     f4.write(str(biblio_senza_orario))
     f4.close
